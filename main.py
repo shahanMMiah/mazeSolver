103cfaaa7f6f2fd58edfca6ae86f91881951a0b9
shahanMMiah <shahan.m.miah@gmail.com>
2024-10-24 23:20:01 +0100

    cell class

--- main.py
+++ main.py
@@ -13,17 +13,18 @@ class Line():
     def draw(self, canvas: Canvas, color = "black"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=color, width=2)
 
-
         
 class Window():
     def __init__(self, width: float, height: float, title):
         
         self.root = Tk()
         self.title = title
-        self.canvas = Canvas(self.root) 
-        self.canvas.width = width
-        self.canvas.height = height
+    
+        self.root.minsize(height, width)       
+        self.canvas = Canvas(self.root)
+        self.canvas.config(width=width, height=height)
         self.canvas.pack()
+
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -41,12 +42,57 @@ class Window():
         
         line.draw(self.canvas, color)
 
+class Cell():
+    def __init__(
+        self, 
+        left: bool, 
+        right: bool, 
+        top: bool,
+        bot: bool, 
+        point: Point, 
+        win: Window
+        ):
+
+        self.left_wall = left
+        self.right_wall = right
+        self.top_wall = top
+        self.bot_wall = bot
+
+        self.pos = point
+        self.win = win
+
+    def draw(self, size = 20):
+        
+        
+        x = self.pos.x
+        y = self.pos.y
+
+        top_line = Line(self.pos, Point(x + size, y))
+        bot_line = Line(Point(x , y+size), Point(x + size, y+size))
+        left_line = Line(self.pos, Point(x , y + size))
+        right_line = Line(Point(x+size , y), Point(x + size, y+size))
+        
+        if self.top_wall:
+            top_line.draw(self.win.canvas)
+        if self.bot_wall:
+            bot_line.draw(self.win.canvas)
+        if self.left_wall:
+            left_line.draw(self.win.canvas)
+        if self.right_wall:
+            right_line.draw(self.win.canvas)
+
+
 
 def main():
-    window = Window(200,200,"test")
+    window = Window(500,500,"test")
 
     line1 = Line(Point(100,0),Point(100, 100))
-    window.draw_line(line1)
+    #window.draw_line(line1)
+
+    test_cell = Cell(True,True,True,True,Point(100,300),window)
+
+    test_cell.draw(100)
+    
     window.wait_for_close()
     print("hello")
 
